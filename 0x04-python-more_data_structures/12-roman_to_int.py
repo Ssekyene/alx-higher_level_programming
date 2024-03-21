152b1aef3d733fa339a0e09623fd8ad0fec7c088
Ssekyene <robertssekyene05@gmail.com>
2024-03-21 13:39:17 -0700

    added a comment

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,6 +6,11 @@ def roman_to_int(roman_string):
     numbers = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     for i, roman in enumerate(roman_string):
         if i != 0 and numbers.get(roman) > numbers.get(roman_string[i - 1]):
+            '''you may check for the result of 
+            (numbers.get(roman) - 2 * numbers.get(roman_string[i - 1]))
+            for the first digit which must be divisble by either
+            4 or 9 to nullify wrong roman numerals
+            '''
             num += (numbers.get(roman) - 2 * numbers.get(roman_string[i - 1]))
         else:
             num += numbers.get(roman)
