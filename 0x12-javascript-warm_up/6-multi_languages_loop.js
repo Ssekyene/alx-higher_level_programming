ca8031d2188bb66f0339b5e34241f077cda7f175
Ssekyene Robert <robertssekyene05@gmail.com>
2024-08-07 23:10:34 +0000

    modified variable name

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
+// prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
 
 const _array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (const a of _array) {
-  console.log(a);
+for (const str of _array) {
+  console.log(str);
 }
