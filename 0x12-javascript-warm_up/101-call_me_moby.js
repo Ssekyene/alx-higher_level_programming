39fe1b9300741243bb52042e199f58483b6a0f26
Ssekyene Robert <robertssekyene05@gmail.com>
2024-08-08 14:42:00 +0000

    fixed file 101-

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-
+// exports a function that executes `x` times a function
 exports.callMeMoby = function (x, theFunction) {
-  for (let count = 0; count < x; i++) {
+  for (let count = 0; count < x; count++) {
     theFunction();
   }
 };
