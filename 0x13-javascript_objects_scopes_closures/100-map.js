2ffc67ae591f26da6480d0c377b00c00754654c0
Ssekyene Robert <robertssekyene05@gmail.com>
2024-08-11 14:03:17 +0000

    corrected the semistandard lint

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+// a script that imports an array and computes a new array
 const list = require('./100-data').list;
 
 const newList = list.map((x, index) => x * index);
--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,16 @@
 #!/usr/bin/node
-
+/*
+  a script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence with the following instructions:
+  Your script must import dict from the file 101-data.js
+  In the new dictionary:
+    - A key is a number of occurrences
+    - A value is the list of user ids
+  Print the new dictionary at the end
+*/
 const dict = require('./101-data').dict;
 
-let newDic = {};
-for (let key in dict) {
+const newDic = {};
+for (const key in dict) {
   if (newDic[dict[key]] === undefined) {
     newDic[dict[key]] = [];
   }
