202181c8852bfdb18eb33a11fa273d7ba180cf9b
Ssekyene Robert <robertssekyene05@gmail.com>
2024-08-12 08:03:32 +0000

    added usage examples

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# URL sends GET request to  URL & displays the body of the response 200
+# URL sends GET request to  URL & displays the body of the response 200. Try ./1-body.sh 0.0.0.0:80/[path]
 curl -sL "$1"
