8f33a2ec3af231bff40eb814412de5e68217acf5
Ssekyene Robert <robertssekyene05@gmail.com>
2024-08-12 10:47:09 +0000

    modified the command

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  request to 0.0.0.0:5000/catch_me; server respond - You got me!
-curl -sLX PUT 0.0.0.0:5000/catch_me -o /dev/null -w 'HTTP/1.1 %{http_code}\\nYou got me!\\n'
+curl -sX PUT -L -d "user_id=98" -H "Origin:HolbertonSchool" "0.0.0.0:5000/catch_me"
