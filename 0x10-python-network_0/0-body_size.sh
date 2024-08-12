e97732eaff8b26d5d23372f5a858be9fb0730d15
Ssekyene Robert <robertssekyene05@gmail.com>
2024-08-12 07:44:53 +0000

    documented file 0-

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends request to URL, & display size of the body of the response
+# Sends request to URL, & display size of the body of the response. try ./0-body_size.sh 0.0.0.0:80
 curl -sI "$1" | grep "Content-Length:" | cut -f2 -d' '
