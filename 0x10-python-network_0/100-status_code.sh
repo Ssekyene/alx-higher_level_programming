f0c734fe501b778ff39cae0d4466d8ecccdbfbfb
Ssekyene Robert <robertssekyene05@gmail.com>
2024-08-12 10:40:05 +0000

    modified more

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends request to URL passed as arg & displays status code of the response
+# sends request to URL passed as arg & displays status code of the response. eg ./100-status_code.sh 0.0.0.0:5000 ; echo ""
 curl -o /dev/null -w '%{http_code}' -sLI "$1"
--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends JSON POST req to URL passed as first arg, & displays body of response
+# sends JSON POST req to URL passed as first arg, & displays body of response. eg ./101-post_json.sh 0.0.0.0:5000/route_json my_json_0 ; echo ""
 curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
