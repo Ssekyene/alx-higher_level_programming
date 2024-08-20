108fc17a5beb3e29576f6d082e07ca32e9fd4604
Ssekyene Robert <robertssekyene05@gmail.com>
2024-08-20 06:34:39 +0000

    modified file 0 and 1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
-// reads and prints the content of a file
+/*
+ reads and prints the content of a file
+ arg1: file path
+*/
 const file = process.argv[2];
 const fs = require('fs');
 fs.readFile(file, 'utf8', (err, data) => {
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
-
+/* writes a string to a file
+   arg1: file path
+   arg2: string to write
+*/
 const file = process.argv[2];
 const string = process.argv[3];
 const fs = require('fs');
-fs.writeFile(file, string, function (err) {
+fs.writeFile(file, string, 'utf-8', function (err) {
   if (err) {
     console.log(err);
   }
