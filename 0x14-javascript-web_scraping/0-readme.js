8e2e973476166114ffd0a71fefecb9e10e022711
Ssekyene Robert <robertssekyene05@gmail.com>
2024-08-20 06:09:30 +0000

    modified file 0-

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
-
+// reads and prints the content of a file
 const file = process.argv[2];
 const fs = require('fs');
-fs.readFile(file, 'utf8', (err, data = err) => {
-  console.log(data);
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
