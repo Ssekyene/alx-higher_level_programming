350ac3af630b95fa77b54c698777bcc11ca28c56
Ssekyene Robert <robertssekyene05@gmail.com>
2024-08-21 05:59:06 +0000

    modified file

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
+/*
+ prints all characters of a Star Wars movie
+ arg1: Movie ID - example: 3 = "Return of the Jedi"
+ Example usage: ./100-starwars_characters.js 3
+ Refer to Star wars API: https://swapi-api.alx-tools.com/
+*/
 const request = require('request');
 const address = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
@@ -6,9 +12,15 @@ request(address, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    const results = JSON.parse(body).characters;
-    for (const i of results) {
-      request(i, (e, r, b) => console.log(JSON.parse(b).name));
+    const addresses = JSON.parse(body).characters;
+    for (const chrAdd of addresses) {
+      request(chrAdd, function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
     }
   }
 });
