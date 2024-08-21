677cbf395721eacc166d2781c4ec9790fc60e634
Ssekyene Robert <robertssekyene05@gmail.com>
2024-08-21 07:58:36 +0000

    documented file 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
+/*
+ prints all characters of a Star Wars movie in the right order
+ arg1: Movie ID - example: 3 = "Return of the Jedi"
+ Example usage: ./101-starwars_characters.js 3
+ Refer to Star Wars API: https://swapi-api.alx-tools.com/
+*/
 const request = require('request');
 const address = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
@@ -6,18 +12,24 @@ request(address, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    const results = JSON.parse(body).characters;
+    const addresses = JSON.parse(body).characters;
     const promises = [];
-    for (const i of results) {
+    for (const chrAdd of addresses) {
       promises.push(
         new Promise(function (resolve, reject) {
-          request(i, (e, r, b) => resolve(JSON.parse(b).name));
+          request(chrAdd, (error, response, body) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(JSON.parse(body).name);
+            }
+          });
         })
       );
     }
-    Promise.all(promises).then((a) => {
-      for (const i of a) {
-        console.log(i);
+    Promise.all(promises).then((all) => {
+      for (const prm of all) {
+        console.log(prm);
       }
     });
   }
